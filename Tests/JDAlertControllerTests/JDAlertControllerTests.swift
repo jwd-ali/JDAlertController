15c1d53d5efb1f1e85e967bee8e19ed53b60e85e
jawad <jawad@digitify.com>
2021-03-07 17:08:42 +0500

    SPM support

---
+++ Tests/JDAlertControllerTests/JDAlertControllerTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import JDAlertController
+
+final class JDAlertControllerTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(JDAlertController().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import JDAlertControllerTests
+
+var tests = [XCTestCaseEntry]()
+tests += JDAlertControllerTests.allTests()
+XCTMain(tests)
